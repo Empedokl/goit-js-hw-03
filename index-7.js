871f79510ec80746d6ef6e35f1da63580daa4aa1
Empedokl <63204651+Empedokl@users.noreply.github.com>
2020-08-08 19:45:10 +0300

    fixed task07

--- index-7.js
+++ index-7.js
@@ -1,51 +1,62 @@
-
 const Transaction = {
-    DEPOSIT: 'deposit',
-    WITHDRAW: 'withdraw',
-  };
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
 
-  const account = {
-    balance: 0,
-    transactions: [],
-  
+const account = {
+  balance: 0,
+  transactions: [],
 
-    createTransaction(amount, type) {
+  createTransaction(amount, type) {
     const id = account.transactions.length;
-    return {id, amount, type}
-    },
+    return { id, amount, type };
+  },
 
-    deposit(amount) {
+  deposit(amount) {
     account.balance += amount;
-    account.transactions.push(this.createTransaction(amount, Transaction.DEPOSIT));
-    },
+    account.transactions.push(
+      this.createTransaction(amount, Transaction.DEPOSIT)
+    );
+  },
 
-    withdraw(amount) {
-        if(amount> this.balance){
-            console.log("недостаточно средств");
-        }
-        this.transactions.push(this.createTransaction(amount, Transaction.WITHDRAW)),
-        this.balance -= amount;
-    },
-  
-    getBalance() {
-        return this.balance;
-    },
-  
-    getTransactionDetails(id) {
-        for (const transaction of this.transactions){
-            if (transaction.id === id){
-                return transaction;
-            }
-        }
-    },
-  
-    getTransactionTotal(type) {
-        let number = 0;
-        for (const transaction of this.transactions){
-            if (transaction.type === type){
-                number += transaction.amount;
-            }
-        }
-        return number;
-    },
-  };
+  withdraw(amount) {
+    if (amount > this.balance) {
+      console.log("недостаточно средств");
+      this.withdraw();
+    }
+    this.transactions.push(
+      this.createTransaction(amount, Transaction.WITHDRAW)
+    ),
+      (this.balance -= amount);
+  },
+
+  getBalance() {
+    return this.balance;
+  },
+
+  getTransactionDetails(id) {
+    for (const transaction of this.transactions) {
+      if (transaction.id === id) {
+        return transaction;
+      }
+    }
+  },
+
+  getTransactionTotal(type) {
+    let number = 0;
+    for (const transaction of this.transactions) {
+      if (transaction.type === type) {
+        number += transaction.amount;
+      }
+    }
+    return number;
+  },
+};
+account.deposit(100);
+console.log(account.getBalance());
+account.withdraw(60);
+console.log(account.getBalance());
+console.log(account.getTransactionDetails(0));
+console.log(account.getTransactionDetails(1));
+console.log(account.getTransactionTotal("deposit"));
+console.log(account.getTransactionTotal("withdraw"));
